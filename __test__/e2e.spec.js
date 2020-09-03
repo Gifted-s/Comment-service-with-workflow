54d1f39c94f9375267ea215e5e58572bd6195023
sunkanmiadewumi@gmail.com <sunkanmiadewumi1@gmail.com>
2020-09-03 19:41:37 +0100

    first commit

---
+++ __test__/e2e.spec.js
@@ -0,0 +1,186 @@
+const axios = require('axios')
+let clearDB = require('../app')
+const uuid = require('uuid').v1
+
+beforeAll(() => {
+
+    axios.defaults.headers.common['Content-Type'] = 'application/json'
+    axios.defaults.validateStatus = function (status) {
+        // Throw only if the status code is greater than or equal to 500
+        return status < 500
+    }
+    
+})
+
+beforeEach(()=>{
+    clearDB()
+})
+let root = 'http://localhost:3000/comment-api'
+describe('adding comments', () => {
+    it('adds a comment to the database', async () => {
+
+        const response = await axios.post(
+            root + '/add-comment',
+            {
+                postId: uuid(),
+                author: 'Adewumi Sunkanmi',
+                text: 'I love this post'
+            }
+        )
+        expect(response.status).toBe(201)
+        const { posted } = response.data
+        const comment = await axios.get(root + '/get-comment/'+posted.id)
+        expect(comment.data.comment).toEqual(posted)
+        // let newDB = commentDB.filter(comment => comment.id !== posted.id)
+        // commentDB = newDB
+    })
+    it('requires comment to contain a postId', async () => {
+        const response = await axios.post(
+            root + '/add-comment',
+            { postId: undefined }
+        )
+        expect(response.status).toBe(400)
+        expect(response.data.error).toBe('Comment must have a post id')
+
+    })
+    it('requires comment to contain an author', async () => {
+        const response = await axios.post(
+            root + '/add-comment',
+            { postId: uuid(), author: undefined }
+        )
+        expect(response.status).toBe(400)
+        expect(response.data.error).toBe('Comment must have an author')
+
+    })
+    it('requires comment to contain a text', async () => {
+        const response = await axios.post(
+            root + '/add-comment',
+            { postId: uuid(), author: 'Adewumi Sunkanmi', text: undefined }
+        )
+        expect(response.status).toBe(400)
+        expect(response.data.error).toBe('Comment must have a text')
+
+    })
+})
+
+
+describe('editing comments', () => {
+    it('edit a comment in the database', async () => {
+        let commentBody = {
+            postId: uuid(),
+            author: 'Adewumi Sunkanmi',
+            text: 'I love this post'
+        }
+        const response = await axios.post(root + '/add-comment', commentBody)
+        const editedComment = {
+            author: 'Adewumi Demola'
+        }
+        const editedResponse = await axios.patch(root + '/edit-comment/' + response.data.posted.id, editedComment)
+        expect(editedResponse.status).toBe(201)
+        expect({ ...response.data.posted, ...editedComment }).toMatchObject(editedResponse.data.comment)
+    })
+    it('must contain an id to edit a comment', async () => {
+    
+        const editedResponse = await axios.patch(root + '/edit-comment/')
+
+        expect(editedResponse.status).toBe(404)
+       
+    })
+
+    it('must return error if comment was not found', async () => {
+    
+        let commentBody = {
+            postId: uuid(),
+            author: 'Adewumi Sunkanmi',
+            text: 'I love this post'
+        }
+        const response = await axios.post(root + '/add-comment', commentBody)
+        const editedComment = {
+            author: 'Adewumi Demola'
+        }
+       
+        const editedResponse = await axios.patch(root + '/edit-comment/' + '123-456-78-sjdjsd-237283', editedComment)
+        expect(editedResponse.status).toBe(400)
+        expect(editedResponse.data.error).toBe('no comment with this id')
+       
+    })
+})
+
+describe('deleting a comment', () => {
+    it('delete a comment in the database', async () => {
+        let commentBody = {
+            postId: uuid(),
+            author: 'Adewumi Sunkanmi',
+            text: 'I love this post'
+        }
+        const response = await axios.post(root + '/add-comment', commentBody)
+      
+        const deletedResponse = await axios.delete(root + '/delete-comment/' + response.data.posted.id)
+        expect(deletedResponse.status).toBe(201)
+        const nofound = await axios.get(root + '/get-comment/'+response.data.posted.id)
+        expect(nofound.data.comment).toBe(undefined)
+    })
+    it('must contain an id to delete a comment', async () => {
+    
+        const editedResponse = await axios.delete(root + '/edit-comment/')
+
+        expect(editedResponse.status).toBe(404)
+       
+    })
+
+    it('must return error if comment was not found', async () => {
+    
+        const editedResponse = await axios.delete(root + '/delete-comment/' + '123-456-78-sjdjsd-237283')
+        expect(editedResponse.status).toBe(400)
+        expect(editedResponse.data.error).toBe('no comment with this id')
+       
+    })
+})
+
+
+
+describe('getting a comment', () => {
+    it('get a comment from the database', async () => {
+        let commentBody = {
+            postId: uuid(),
+            author: 'Adewumi Sunkanmi',
+            text: 'I love this post'
+        }
+        const response = await axios.post(root + '/add-comment', commentBody)
+        const comment = await axios.get(root + '/get-comment/'+response.data.posted.id)
+        expect(comment.data.comment).toBeDefined()
+    })
+    it('must contain an id to get a comment', async () => {
+    
+        const editedResponse = await axios.patch(root + '/edit-comment/')
+
+        expect(editedResponse.status).toBe(404)
+       
+    })
+
+    it('must return error if comment was not found', async () => {
+    
+        const editedResponse = await axios.get(root + '/get-comment/' + '123-456-78-sjdjsd-237283')
+        expect(editedResponse.status).toBe(400)
+        expect(editedResponse.data.error).toBe('no comment with this id')
+       
+    })
+})
+
+describe('getting all comments', () => {
+    it('get a comment from the database', async () => {
+        let commentBody = {
+            postId: uuid(),
+            author: 'Adewumi Sunkanmi',
+            text: 'I love this post'
+        }
+         await axios.post(root + '/add-comment', commentBody)
+         await axios.post(root + '/add-comment', commentBody)
+         await axios.post(root + '/add-comment', commentBody)
+         await axios.post(root + '/add-comment', commentBody)
+         await axios.post(root + '/add-comment', commentBody)
+         await axios.post(root + '/add-comment', commentBody)
+        let comments= await axios.get(root + '/get-comments/')
+        expect(comments.data.comments.length).toBe(6)
+    })
+})
---
+++ app.js
@@ -0,0 +1,90 @@
+// A simple comment app
+const express = require('express')
+const app = express()
+const cors = require('cors')
+let commentDB = []
+const uuid = require('uuid').v1
+app.use(express.json())
+app.use(cors())
+let rootEndpoint = '/comment-api'
+let port = process.env.PORT || 3000
+app.listen(port)
+app.post(rootEndpoint + '/add-comment', (req, res, next) => {
+
+    let comment = req.body
+    if (!comment.postId) {
+        return res.status(400).send({ error: 'Comment must have a post id' })
+    }
+    if (!comment.author) {
+        return res.status(400).send({ error: 'Comment must have an author' })
+    }
+    if (!comment.text) {
+        return res.status(400).send({ error: 'Comment must have a text' })
+    }
+    else {
+        comment.id = uuid()
+        comment.dateAdded = Date.now()
+        comment.dateModified = Date.now()
+    }
+    commentDB.push(comment)
+    return res.status(201).send({ status: 'success', posted: comment })
+})
+
+app.patch(rootEndpoint + '/edit-comment/:id', (req, res) => {
+    let { id } = req.params
+    let commentBody = req.body
+    if (!id) {
+        return res.status(400).send({ error: 'id is required' })
+    }
+    else {
+        let comment = commentDB.find((comment) => comment.id === id)
+        if (!comment) {
+            return res.status(400).send({ error: 'no comment with this id' })
+        }
+        comment = { ...comment, ...commentBody }
+        comment.dateModified = Date.now()
+        return res.status(201).send({ message: 'comment edited', comment })
+    }
+})
+
+app.delete(rootEndpoint + '/delete-comment/:id', (req, res) => {
+    let { id } = req.params
+    if (!id) {
+       return res.status(400).send({ error: 'id is required' })
+    }
+    let comment = commentDB.find((comment) => comment.id === id)
+    if (!comment) {
+        return res.status(400).send({ error: 'no comment with this id' })
+    }
+    else {
+        let newComments = commentDB.filter((comment) => comment.id !== id)
+        commentDB = newComments
+       return res.status(201).send({ message: 'comment deleted', commentDB })
+    }
+})
+
+app.get(rootEndpoint + '/get-comment/:id', (req, res) => {
+    let { id } = req.params
+    if (!id) {
+        res.status(400).send({ error: 'id is required' })
+    }
+    else {
+        let comment = commentDB.find((comment) => comment.id === id)
+        if (!comment) {
+            res.status(400).send({ error: 'no comment with this id' })
+        }
+        else {
+            res.status(201).send({ comment })
+        }
+
+    }
+})
+
+app.get(rootEndpoint + '/get-comments', (req, res) => {
+    res.status(201).send({comments: commentDB })
+})
+
+function clearDB(){
+    commentDB=[]
+}
+module.exports = clearDB
